c0f8c795d260f651e451fe4b3721204429cfbeb7
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-04 20:01:19 -0600

    Add: Anonymous funciton

--- index.php
+++ index.php
@@ -1,5 +1,38 @@
 <?php
-/* !Variable Funtions! */
+/* !Variable Functions! */
 
+/*
+function answer(){
+    return 42;
+}
+
+function add_up($a, $b){
+    return $a + $b;
+}
+
+//Pass function string into variable 
+$func = 'answer';
+
+//Use variable to call answer() function
+//Echo 42
+echo $func();
+
+//Pass function string into variable
+$funct = 'add_up';
+
+//Pass arguments to function variable
+echo $funct(2,2);
+*/
+
+/* !Anonymous Functions! */
+
+$name = 'Mike';
+
+//Use a variable outside of the function scope inside of the function
+$greet = function() use($name){
+    echo "Hello, $name!";
+};
+
+$greet();
 
 ?>
